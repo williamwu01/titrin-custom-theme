5d6f524831fa008a17dbf66fdf7c47a72a533ed2
William Wu <williamwuu3@gmail.com>
2025-04-25 01:16:58 -0700

    css complete for about page and blog page

--- front-page.php
+++ front-page.php
@@ -91,15 +91,35 @@ get_header();
 					<label for="cf-email">Email</label>
 					<input type="email" id="cf-email" name="cf-email" required>
 
+					<label for="cf-phone">Phone</label>
+					<input type="tel" id="cf-phone" name="cf-phone">
+					<!-- Optional field for phone number -->
+
 					<label for="cf-message">Message</label>
 					<textarea id="cf-message" name="cf-message" required></textarea>
 
 					<input type="submit" name="cf-submitted" value="Send">
 				</form>
 
-				<?php if (isset($_POST['cf-submitted'])): ?>
-					<p class="contact-message">Thank you for contacting us. We will be in touch soon.</p>
-				<?php endif; ?>
+				<?php if (isset($_POST['cf-submitted'])){
+
+					$name = sanitize_text_field($_POST['cf-name']);
+					$email = sanitize_email($_POST['cf-email']);
+					$message = sanitize_textarea_field($_POST['cf-message']);
+					$phone = sanitize_text_field($_POST['cf-phone']);
+
+					$to = get_option('admin_email');
+					$subject = 'New Contact Form Submission';
+					$body    = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message";
+					$headers = array('Content-Type: text/html; charset=UTF-8');
+
+					if (wp_mail($to, $subject, $body, $headers)) {
+						echo '<p class="contact-message">Thank you for contacting us. We will be in touch soon.</p>';
+					} else {
+						echo '<p class="contact-error">There was an error sending your message. Please try again later.</p>';
+					}
+				} 			
+				?>
 			</div>
 		</div>
 	</section>
